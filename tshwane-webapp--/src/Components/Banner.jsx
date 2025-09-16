affd4f7dc6d90436d79321de7d654404ac51e043
Prince <juniortlou212@gmail.com>
2025-09-16 16:17:25 +0200

    Created something basic and ran it

---
+++ tshwane-webapp--/src/Components/Banner.jsx
@@ -0,0 +1,11 @@
+import "./banner.css"
+
+function Banner (){
+    return (
+        <div className="banner_title"> 
+            <h1>Tshwane Bus Route</h1>
+        </div>
+    )
+
+}
+export default Banner;
