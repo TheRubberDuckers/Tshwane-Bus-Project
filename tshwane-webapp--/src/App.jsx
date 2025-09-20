57495cb22dadf376de446d2a4262c0f9ac66e258
LindelaniMKH <dlaminlindelani.15@gmail.com>
2025-09-20 13:37:03 +0200

    Deleted files from UI branch

--- tshwane-webapp--/src/App.jsx
+++ tshwane-webapp--/src/App.jsx
@@ -3,24 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Map from './Map-Component'
 import './App.css'
-import Banner from "./Components/Banner";
+import MapComponent from './Map-Component'
+
 
 
 function App() {
-return (
-<div>
-    <Banner />
-    <p>Welcome to the City Of Tshwane Bus route</p>
-</div>
-);
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div id='MapContainer'>
-        <MapComponent></MapComponent>
-      </div>
-    </>
+return (
+  <>
+    <MapComponent></MapComponent>
+  </>
   )
 }
 export default App;
