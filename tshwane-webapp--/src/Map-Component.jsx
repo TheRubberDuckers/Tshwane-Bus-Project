03d6db8ecc01bd54fa9a381d8893b9838307f1c9
Prince <juniortlou212@gmail.com>
2025-09-24 16:29:01 +0200

    Revert "Made the map-component only render the map and nothing else"
    This reverts commit 355c27b05a0c1db3069a05df6576de1da9995e48.
    
    I reverted back cause i broke it

--- tshwane-webapp--/src/Map-Component.jsx
+++ tshwane-webapp--/src/Map-Component.jsx
@@ -1,8 +1,20 @@
-import { MapContainer, TileLayer, } from 'react-leaflet'
+import { MapContainer, TileLayer, Polyline, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import Map from './assets/Map/Map.json'
 
 function MapComponent(){
-    firstPos=[-25.731340, 28.218370];
+    const route = [];
+    const arrLen = Map.features[0]['geometry']['coordinates'].length;
+    const coordinates = Map.features[0]['geometry']['coordinates'];
+
+    for (let i = 0; i < arrLen; i++){
+        const reversed = [...coordinates[i]].reverse(); //make a copy of coordinates array and then revese that copy
+        route.push(reversed);
+    }
+
+    const firstPos = route[0];
+    const secondPos = route[route.length - 1];
+
     return(
         <>
             <MapContainer center={firstPos} zoom={14} scrollWheelZoom={false} style={{ height: "60vh", width: "60vw" }}>
@@ -10,6 +22,15 @@ function MapComponent(){
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <Marker position={firstPos}>
+                <Popup>
+                    Found near macdonald 
+                </Popup>
+            </Marker>
+            <Marker position={secondPos}>
+                <Popup>Second Message</Popup>
+            </Marker>
+            <Polyline positions={route} ></Polyline>
             </MapContainer>
         </>
     )
