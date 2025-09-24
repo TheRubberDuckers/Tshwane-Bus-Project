355c27b05a0c1db3069a05df6576de1da9995e48
Prince <juniortlou212@gmail.com>
2025-09-24 15:49:36 +0200

    Made the map-component only render the map and nothing else

--- tshwane-webapp--/src/Map-Component.jsx
+++ tshwane-webapp--/src/Map-Component.jsx
@@ -1,20 +1,8 @@
-import { MapContainer, TileLayer, Polyline, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
-import Map from './assets/Map/Map.json'
 
 function MapComponent(){
-    const route = [];
-    const arrLen = Map.features[0]['geometry']['coordinates'].length;
-    const coordinates = Map.features[0]['geometry']['coordinates'];
-
-    for (let i = 0; i < arrLen; i++){
-        const reversed = [...coordinates[i]].reverse(); //make a copy of coordinates array and then revese that copy
-        route.push(reversed);
-    }
-
-    const firstPos = route[0];
-    const secondPos = route[route.length - 1];
-
+    firstPos=[-25.731340, 28.218370];
     return(
         <>
             <MapContainer center={firstPos} zoom={14} scrollWheelZoom={false} style={{ height: "60vh", width: "60vw" }}>
@@ -22,15 +10,6 @@ function MapComponent(){
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={firstPos}>
-                <Popup>
-                    Found near macdonald 
-                </Popup>
-            </Marker>
-            <Marker position={secondPos}>
-                <Popup>Second Message</Popup>
-            </Marker>
-            <Polyline positions={route} ></Polyline>
             </MapContainer>
         </>
     )
