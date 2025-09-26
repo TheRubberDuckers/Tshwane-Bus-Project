48f7de6243ed2995cb9b059ba59fa29103380295
Prince <juniortlou212@gmail.com>
2025-09-26 19:25:11 +0200

    Fixed minor issues

--- tshwane-webapp--/src/Map-Component.jsx
+++ tshwane-webapp--/src/Map-Component.jsx
@@ -1,5 +1,6 @@
-import { MapContainer, TileLayer, Polyline, Popup,} from 'react-leaflet'
+import { MapContainer, TileLayer, Polyline, Popup,Marker} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import React from 'react';
 
 function MapComponent({routes, activeRoute}){
     console.log (routes)
@@ -7,21 +8,28 @@ function MapComponent({routes, activeRoute}){
     return(
         
         <>
-            <MapContainer center={[-25.7461, 28.1881]} zoom={14} scrollWheelZoom={false} style={{ height: "60vh", width: "60vw" }}>
+            <MapContainer center={[-25.7461, 28.1881]} zoom={14} touchZoom={true} scrollWheelZoom={true} style={{ height: "60vh", width: "60vw" }}>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             {routes.map(route=>
                 (
+                    
                     activeRoute==route.id && (
-                        <Polyline
-                        key = {route.id}
-                        positions = {route.coordinates}
-                        color={route.color}
-                        >
-                            <Popup>{route.name}</Popup>
-                        </Polyline>
+                        <React.Fragment key={route.id}>
+                            <Polyline
+                            positions = {route.coordinates}
+                            color={route.color}
+                            >
+                            </Polyline>
+                            <Marker position={route.coordinates[0]}>
+                                <Popup>{route.name} Start</Popup>
+                            </Marker>
+                            <Marker position={route.coordinates[route.coordinates.length -1]}>
+                                <Popup>{route.name} End</Popup>
+                            </Marker>
+                        </React.Fragment>
                     )
                 )
             )}
