ed890f8d9d9a55d12dd63864ae310cf1ba0da37b
LindelaniMKH <dlaminlindelani.15@gmail.com>
2026-03-19 21:03:37 +0200

    Installed TailWindCSS

--- tshwane-webapp--/src/Components/Route_line.jsx
+++ tshwane-webapp--/src/Components/Route_line.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import { Polyline, Marker } from "leaflet";
 
-export default function Route_line(data, colors, name){
-  const [active, setActive]=useState(false);
-  return(
-  <div>
-    <button onClick= {
-      () => setActive(!active)
-    }>
-      {active ? {name} : {name}}
-    </button>
-    {active && <Polyline positions={data} color={colors}/>}
-    {active && <marker position = {data[0]} />}
-    {active && <marker position = {data[-1]} />}
-  </div>
+export default function Route_line(data, colors, name) {
+  const [active, setActive] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setActive(!active)}>
+        {active ? { name } : { name }}
+      </button>
+      {active && <Polyline positions={data} color={colors} />}
+      {active && <marker position={data[0]} />}
+      {active && <marker position={data[-1]} />}
+    </div>
   );
 }
